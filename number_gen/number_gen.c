a2b8aae6dd4490c9b2183c8989a7ca552091e9a9
Darío <dario.mart.lop.03@gmail.com>
2025-04-15 04:57:20 +0200

    updated random number generator

--- number_gen/number_gen.c
+++ number_gen/number_gen.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_NUM 10000  // Rango máximo para los números aleatorios
 
@@ -17,55 +18,63 @@ bool existe(int array[], int tamaño, int num) {
 
 int main() {
     int cantidad;
+    int repeat;
 
     printf("¿Cuántos números aleatorios únicos deseas generar? ");
     scanf("%d", &cantidad);
 
-    if (cantidad > MAX_NUM) {
-        printf("Error: No se pueden generar más de %d números únicos dentro del rango.\n", MAX_NUM);
-        return 1;
-    }
+    printf("¿Cuántas veces deseas regenerar números? ");
+    scanf("%d", &repeat);
 
-    int *numeros = malloc(cantidad * sizeof(int));
-    if (numeros == NULL) {
-        printf("Error al asignar memoria.\n");
-        return 1;
-    }
-
-    srand(time(NULL)); // Semilla basada en el tiempo
-
-    int generados = 0;
-    while (generados < cantidad) {
-        int num = rand() % MAX_NUM;
-        if (!existe(numeros, generados, num)) {
-            numeros[generados] = num;
-            generados++;
+    while (repeat > 0)
+    {
+        if (cantidad > MAX_NUM) {
+            printf("Error: No se pueden generar más de %d números únicos dentro del rango.\n", MAX_NUM);
+            return 1;
         }
-    }
 
-    char *resultado = malloc(cantidad * 6);
-    if (resultado == NULL) {
-        printf("Error al asignar memoria para la cadena.\n");
+        int *numeros = malloc(cantidad * sizeof(int));
+        if (numeros == NULL) {
+            printf("Error al asignar memoria.\n");
+            return 1;
+        }
+
+        srand(time(NULL)); // Semilla basada en el tiempo
+
+        int generados = 0;
+        while (generados < cantidad) {
+            int num = rand() % MAX_NUM;
+            if (!existe(numeros, generados, num)) {
+                numeros[generados] = num;
+                generados++;
+            }
+        }
+
+        char *resultado = malloc(cantidad * 6);
+        if (resultado == NULL) {
+            printf("Error al asignar memoria para la cadena.\n");
+            free(numeros);
+            return 1;
+        }
+
+        resultado[0] = '\0'; // Inicializar cadena vacía
+
+        for (int i = 0; i < cantidad; i++) {
+            char buffer[16];
+            snprintf(buffer, sizeof(buffer), "%d ", numeros[i]);
+            strcat(resultado, buffer);
+        }
+
+        char comando[100000]; // Asegúrate de que tenga suficiente espacio
+        snprintf(comando, sizeof(comando), ".././push_swap \"%s\" | wc -l", resultado);
+        system(comando);
+        snprintf(comando, sizeof(comando), ".././push_swap \"%s\" | .././checker_linux \"%s\"", resultado, resultado);
+        system(comando);
+
+        write(1, "-----------\n", 13);
+
         free(numeros);
-        return 1;
+        repeat--;
     }
-
-    resultado[0] = '\0'; // Inicializar cadena vacía
-
-    for (int i = 0; i < cantidad; i++) {
-        char buffer[16];
-        snprintf(buffer, sizeof(buffer), "%d ", numeros[i]);
-        strcat(resultado, buffer);
-    }
-
-    printf("Cadena de números generada");
-
-	char comando[100000]; // Asegúrate de que tenga suficiente espacio
-	snprintf(comando, sizeof(comando), ".././push_swap \"%s\" | wc -l", resultado);
-	system(comando);
-	snprintf(comando, sizeof(comando), ".././push_swap \"%s\" | .././checker_linux \"%s\"", resultado, resultado);
-	system(comando);
-
-    free(numeros);
     return 0;
 }
